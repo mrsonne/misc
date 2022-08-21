177867a43eb2da403b077e94edafca554146d127
Jacob Sonne <mrsonne@gmail.com>
2022-08-21 20:13:52 +0200

    improve plotting

--- run_two_models.py
+++ run_two_models.py
@@ -1,19 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-from two_models import generate_data, plot, model
+from two_models import generate_data, plot, model, plot_parameters
 
-np.random.seed(23)
+np.random.seed(0)
+sigma = 0.5
+irow_1_model = 0
+irow_2_models = 1
+print(f"sigma {sigma}")
 
 xmin, xmax = -1, 1
 pars_model1 = 1.0, 1.0
-sigma_model1 = 0.1
+sigma_model1 = sigma
 n_model1 = 10
 xs_model1 = np.linspace(xmin, xmax, n_model1)
 ys_model1 = generate_data(xs_model1, pars_model1, sigma_model1)
 
 pars_model2 = -1.0, 1.0
-sigma_model2 = 0.1
+sigma_model2 = sigma
 n_model2 = 10
 xs_model2 = np.linspace(xmin, xmax, n_model2)
 ys_model2 = generate_data(xs_model2, pars_model2, sigma_model2)
@@ -22,12 +26,31 @@ xs_all = np.concatenate((xs_model1, xs_model2))
 ys_all = np.concatenate((ys_model1, ys_model2))
 
 fig, axs = plt.subplots(2, 2, figsize=(16, 12), sharex=True)
-plot(xs_model1, ys_model1, axs[0, 0], marker="x", color="blue", label=f"Data model 1")
 plot(
-    xs_model2, ys_model2, axs[0, 0], marker="x", color="magenta", label=f"Data model 2"
+    xs_model1,
+    ys_model1,
+    axs[irow_2_models, 0],
+    marker="x",
+    color="blue",
+    label=f"Data model 1",
+)
+plot(
+    xs_model2,
+    ys_model2,
+    axs[irow_2_models, 0],
+    marker="x",
+    color="magenta",
+    label=f"Data model 2",
 )
 
-plot(xs_all, ys_all, axs[1, 0], marker="o", color="red", label=f"Data models 1 & 2")
+plot(
+    xs_all,
+    ys_all,
+    axs[irow_1_model, 0],
+    marker="o",
+    color="red",
+    label=f"Data models 1 & 2",
+)
 
 # Fit the data
 popt_model1, pcov1 = curve_fit(model, xs_model1, ys_model1)
@@ -35,8 +58,13 @@ popt_model2, pcov2 = curve_fit(model, xs_model2, ys_model2)
 popt_all, pcov_all = curve_fit(model, xs_all, ys_all)
 
 print(popt_model1, pcov1)
-print(popt_model1, pcov2)
+print(popt_model2, pcov2)
 print(popt_all, pcov_all)
+
+plot_parameters(popt_model1, pcov1, axs[irow_2_models, 1], "Data model 1")
+plot_parameters(popt_model2, pcov2, axs[irow_2_models, 1], "Data model 2")
+plot_parameters(popt_all, pcov_all, axs[irow_1_model, 1], "Data models 1 & 2")
+
 
 ys_predicted_model1 = model(xs_model1, *popt_model1)
 ys_predicted_model2 = model(xs_model2, *popt_model2)
@@ -45,30 +73,30 @@ ys_predicted_all = model(xs_all, *popt_all)
 plot(
     xs_model1,
     ys_predicted_model1,
-    axs[0, 0],
+    axs[irow_2_models, 0],
     marker="",
     linewidth=1,
     color="blue",
-    label=f"model 1 {popt_model1}",
+    label=f"Model 1",
 )
 plot(
     xs_model2,
     ys_predicted_model2,
-    axs[0, 0],
+    axs[irow_2_models, 0],
     marker="",
     linewidth=1,
     color="magenta",
-    label=f"model 2 {popt_model2}",
+    label=f"Model 2",
 )
 
 plot(
     xs_all,
     ys_predicted_all,
-    axs[1, 0],
+    axs[irow_1_model, 0],
     marker="",
     linewidth=1,
     color="red",
-    label=f"model {popt_all}",
+    label=f"Model",
 )
 
 for ax in axs.flat:
--- two_models.py
+++ two_models.py
@@ -1,4 +1,5 @@
 import numpy as np
+from percentiles2d import plot_cov_ellipse
 
 
 def model(x, a, b):
@@ -21,3 +22,30 @@ def plot(xpoints, ypoints, ax, marker="x", color="blue", linewidth=0, label=""):
         label=label,
         markerfacecolor="none",
     )
+
+
+def plot_parameters(popt, pcov, ax, name, idx_x=0, idx_y=1):
+    ax.plot(
+        popt[idx_x],
+        popt[idx_y],
+        "o",
+        label=f"MLE {name}: {np.array2string(popt, precision=2)}",
+        zorder=999,
+        color="green",
+    )
+
+    nstds = [1, 2]
+
+    for nstd in nstds:
+        ellip = plot_cov_ellipse(
+            pcov,
+            popt,
+            ax=ax,
+            nstd=nstd,
+            label=f"MLE COV @ {nstd}x std",
+            fill=None,
+            edgecolor="green",
+            linewidth=1,
+            zorder=999,
+        )
+        ellip.set(alpha=0.5)

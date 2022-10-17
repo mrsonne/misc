28e67ef6afd9af87a1a294cb58692bf38a067fa1
Jacob Sonne <mrsonne@gmail.com>
2022-10-17 14:18:53 +0200

    it sort of works

--- run_model_classification.py
+++ run_model_classification.py
@@ -1,11 +1,17 @@
 """
 Attempt to classify data in term of which model created the data and learn the models.
 Should also attempt to figure out how many models is best (AIC or BIC)
+
+Resources:
+    https://towardsdatascience.com/gaussian-mixture-models-explained-6986aaf5a95
+
+n_models = n_features = n_components
 """
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-from two_models import make_data, plot, model, plot_parameters
+from sklearn.mixture import GaussianMixture
+from two_models import make_data, plot, model, plot_parameters, plot_cov_ellipse
 
 np.random.seed(0)
 irow_1_model = 0
@@ -34,14 +40,13 @@ plot(
 print(xs_all.shape)
 data = np.transpose(np.vstack((xs_all, ys_all)))
 print(data.shape)
-np.random.shuffle(data)
-nmodels = 2
-sets = np.split(data, nmodels)
-print(sets)
-
+# np.random.shuffle(data)
+n_models = 2
+sets = np.split(data, n_models)
 
 colors = "red", "blue"
-ssrs = []
+# Sum of squared residualts
+rs = []
 for iset, (set, color) in enumerate(zip(sets, colors)):
     plot(
         set[:, 0],
@@ -51,17 +56,122 @@ for iset, (set, color) in enumerate(zip(sets, colors)):
         label=f"Set {iset}",
     )
 
-    popt, _ = curve_fit(model, set[:, 0], set[:, 1])
-    ys_all_pred = model(xs_all, *popt)
-    ssr = (ys_all - ys_all_pred) ** 2
-    print(ssr)
-    ssrs.append(ssr)
+    plot(
+        set[:, 0],
+        set[:, 1],
+        axs[1, 0],
+        color=color,
+        label=f"Set {iset}",
+    )
 
-axs[0, 1].plot(ssrs[0], ssrs[1], "o")
-axs[0, 1].set_xlabel("SSR model 1")
-axs[0, 1].set_ylabel("SSR model 2")
+    plot(
+        set[:, 0],
+        set[:, 1],
+        axs[1, 1],
+        color=color,
+        label=f"Set {iset}",
+    )
+
+    # Fit model for current set
+    popt, _ = curve_fit(model, set[:, 0], set[:, 1])
+
+    # Predict for all data point using current model
+    ys_all_pred = model(xs_all, *popt)
+
+    # Plot prediction on original data split correctly into the two models
+    plot(
+        xs_all,
+        ys_all_pred,
+        axs[0, 0],
+        linewidth=1,
+        color=color,
+        label=f"Model: {popt}",
+    )
+
+    plot(
+        xs_all,
+        ys_all_pred,
+        axs[1, 1],
+        linewidth=1,
+        color=color,
+        label=f"Model real split: {popt}",
+    )
+
+    # Collect residual for all data point in current model
+    r = (ys_all - ys_all_pred) ** 2
+    # r = ys_all - ys_all_pred
+    rs.append(r)
+
+#  (n_samples, n_features)
+X = np.stack(rs, axis=1)
+print(X.shape)
+gm = GaussianMixture(n_components=n_models, random_state=0)
+labels = gm.fit_predict(X)
+label_ids = range(n_models)
+
+proba = gm.predict_proba(X)
+print(proba)
+
+
+# shape: (n_components, n_features)
+means = gm.means_
+covs = gm.covariances_
+
+for mean, cov, color, label in zip(means, covs, colors, label_ids):
+
+    # Plot GMM elipses
+    axs[0, 1].plot(mean[0], mean[1], "+k", markersize=20)
+    plot_cov_ellipse(
+        cov, mean, nstd=2, ax=axs[0, 1], color=color, zorder=-1000, alpha=0.3
+    )
+
+    # Plot GMM input data colored according to predicted classes
+    axs[0, 1].plot(rs[0][labels == label], rs[1][labels == label], "o", color=color)
+
+    # Plot original data with label color
+    xs_ = xs_all[labels == label]
+    ys_ = ys_all[labels == label]
+    popt, _ = curve_fit(model, xs_, ys_)
+
+    axs[1, 0].plot(
+        xs_,
+        ys_,
+        "o",
+        markersize=15,
+        alpha=0.2,
+        color=color,
+        label=f"Predicted label: {label}",
+    )
+
+    ys_pred = model(xs_, *popt)
+
+    # Plot prediction on original data split according to predicted classes
+    axs[1, 1].plot(
+        xs_,
+        ys_pred,
+        linestyle="dashed",
+        linewidth=1,
+        color=color,
+        label=f"Model cls split: {popt}",
+    )
+
+    axs[1, 0].plot(
+        xs_,
+        ys_pred,
+        linestyle="dashed",
+        linewidth=1,
+        color=color,
+        label=f"Model: {popt}",
+    )
+
+
+axs[0, 1].set_xlabel("R model 1")
+axs[0, 1].set_ylabel("R model 2")
+
+for ax in axs.flat:
+    ax.legend()
 
 # Make clustering
 
 fig.tight_layout()
-fig.savefig("two-models-cls.png")
+fig.savefig("two-models-cls.png", dpi=180)

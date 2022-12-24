6722fe5428ba0af084724dad573a537cdff5635e
Jacob Sonne <mrsonne@gmail.com>
2022-12-24 03:29:22 +0100

    refactor

--- regmix.py
+++ regmix.py
@@ -1,13 +1,72 @@
 # %% Imports
 
-# https://stats.stackexchange.com/questions/185812/regression-mixture-in-pymc3
-
 
 ##Fake data
 import numpy as np
 import matplotlib.pyplot as plt
 import pymc3 as pm
+
 import theano.tensor as tt
+
+
+def c3(X1, Y):
+    ...
+
+
+def c2(X1, Y):
+    """Two component model
+    Modified from https://stats.stackexchange.com/questions/185812/regression-mixture-in-pymc3
+    """
+    n_components = 2
+    size = X1.size
+    basic_model = pm.Model()
+    with basic_model:
+        p = pm.Uniform("p", 0, 1)  # Proportion in each mixture
+
+        alpha = pm.Normal("alpha", mu=0, sd=10)  # Intercept
+        beta_1 = pm.Normal(
+            "beta_1", mu=0, sd=100, shape=n_components
+        )  # Betas.  Two of them.
+        sigma = pm.Uniform("sigma", 0, 20)  # Noise
+
+        # https://docs.pymc.io/en/v3/pymc-examples/examples/mixture_models/gaussian_mixture_model.html
+        # Break the symmetry. beta_1 array should always be sorted in
+        # increasing order so it's easier to compare and average over chains
+
+        if n_components > 1:
+            switches = tt.switch(beta_1[1] - beta_1[0] < 0, -np.inf, 0)
+            order_slopes_potential = pm.Potential(
+                "order_slopes_potential",
+                switches,
+            )
+
+        category = pm.Bernoulli(
+            "category", p=p, shape=size
+        )  # Classification of each observation
+
+        b1 = pm.Deterministic("b1", beta_1[category])  # Choose beta based on category
+
+        mu = alpha + b1 * X1  # Expected value of outcome
+
+        # Likelihood
+        Y_obs = pm.Normal("Y_obs", mu=mu, sd=sigma, observed=Y)
+
+    with basic_model:
+        step1 = pm.Metropolis([p, alpha, beta_1, sigma])
+        # non-default scaling is important
+        step2 = pm.BinaryMetropolis([category], scaling=0.01)
+
+        # the classification part converges to different categories
+        trace = pm.sample(
+            20000,
+            [step1, step2],
+            progressbar=True,
+            return_inferencedata=False,
+            start={"beta_1": np.linspace(-1, 1, n_components)},  #
+        )
+
+    return trace
+
 
 # import seaborn as sns
 
@@ -15,74 +74,33 @@ import theano.tensor as tt
 
 np.random.seed(123)
 alpha = 0
-sigma = 1
-beta = [-5]
+sigma = 2
+beta1 = [-5]
 beta2 = [5]
 # size = 250
-size = 50
+size1 = 25
+size2 = 35
 
 # Predictor variable
 # X1_1 = np.random.randn(size)
-X1_1 = np.linspace(-2, 2, size)
+X1_1 = np.linspace(-2, 2, size1)
 
 # Simulate outcome variable--cluster 1
-Y1 = alpha + beta[0] * X1_1 + np.random.normal(loc=0, scale=sigma, size=size)
+Y1 = alpha + beta1[0] * X1_1 + np.random.normal(loc=0, scale=sigma, size=size1)
 
 # Predictor variable
 # X1_2 = np.random.randn(size)
-X1_2 = np.linspace(-2, 2, size)
+X1_2 = np.linspace(-2, 2, size2)
 # Simulate outcome variable --cluster 2
-Y2 = alpha + beta2[0] * X1_2 + np.random.normal(loc=0, scale=sigma, size=size)
+Y2 = alpha + beta2[0] * X1_2 + np.random.normal(loc=0, scale=sigma, size=size2)
 
 
 X1 = np.append(X1_1, X1_2)
 Y = np.append(Y1, Y2)
 
 
-# %% Model
-
-basic_model = pm.Model()
-
-with basic_model:
-    p = pm.Uniform("p", 0, 1)  # Proportion in each mixture
-
-    alpha = pm.Normal("alpha", mu=0, sd=10)  # Intercept
-    beta_1 = pm.Normal("beta_1", mu=0, sd=100, shape=2)  # Betas.  Two of them.
-    sigma = pm.Uniform("sigma", 0, 20)  # Noise
-
-    # https://docs.pymc.io/en/v3/pymc-examples/examples/mixture_models/gaussian_mixture_model.html
-    # Break the symmetry. beta_1 array should always be sorted in
-    # increasing order so it's easier to compare and average over chains
-    order_slopes_potential = pm.Potential(
-        "order_slopes_potential", tt.switch(beta_1[1] - beta_1[0] < 0, -np.inf, 0)
-    )
-
-    category = pm.Bernoulli(
-        "category", p=p, shape=size * 2
-    )  # Classification of each observation
-
-    b1 = pm.Deterministic("b1", beta_1[category])  # Choose beta based on category
-
-    mu = alpha + b1 * X1  # Expected value of outcome
-
-    # Likelihood
-    Y_obs = pm.Normal("Y_obs", mu=mu, sd=sigma, observed=Y)
-
-
-with basic_model:
-    step1 = pm.Metropolis([p, alpha, beta_1, sigma])
-    # non-default scaling is importtant
-    step2 = pm.BinaryMetropolis([category], scaling=0.01)
-
-    # the classification part converges to different categories
-    trace = pm.sample(
-        20000,
-        [step1, step2],
-        progressbar=True,
-        return_inferencedata=False,
-        start={"beta_1": [-1, 1]},  #
-    )
-
+# %% Run model
+trace = c2(X1, Y)
 
 # %% post simulation calcs
 # print(trace)
@@ -114,7 +132,7 @@ fig.savefig("regmix-trace.png")
 # %% Plotting: results
 
 # this mean averages "how many times in the trace
-# a point is averaged as either 0 or 1"
+# a point is categorized as either 0 or 1"
 # the mean of the mean should give some center value that can be used to categorize
 p_cat = np.apply_along_axis(np.mean, 0, trace["category"])
 cat = p_cat - np.mean(p_cat) < 0
